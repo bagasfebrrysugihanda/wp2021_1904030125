cefe03a783741f5a02cac276f5ba05965b674699
bagasfebrrysugihanda <1904030125@students.unis.ac.id>
2022-01-05 00:12:58 +0700

    menambahkan file pertemuan 3

---
+++ Biodata/identitas.php
@@ -0,0 +1,23 @@
+<?php
+$namadepan = "Bagas";
+$namablkng = "Febrry Sugihanda";
+$matakuliah = "Web Programing";
+$nim = "1904030125";
+$hoby = "Membaca Novel";
+$Usia = (0x13);
+$Alamat = "Kp.Ketos 002/001";
+$Jeniskelamin = "Laki-Laki";
+$tempattanggallahir = "Tangerang, 11-02-2002";
+$Notelphone = "081386743084";
+?>
+
+<h4>Identitas diri</h4>
+<p>Nama : <?php echo $namadepan . " " . $namablkng; ?></p>
+<p>Mata kuliah : <?php echo $matakuliah; ?></p>
+<p>NIM : <?php echo $nim ?></p>
+<p>Hobi : <?php echo $hoby ?></p>
+<p>Umur : <?php echo $Usia ?></p>
+<p>Alamat : <?php echo $Alamat ?></p>
+<p>Jenis Kelamin : <?php echo $Jeniskelamin ?></p>
+<p> Tempat Tanggal Lahir : <?php echo $tempattanggallahir ?></p>
+<p>Nomer hp : <?php echo $Notelphone ?></p>
